f02a4b99d885d3f4f317f6fd83a2f0589a8b4ed8
loneil1 <oneil779@umn.edu>
2023-06-11 15:44:29 -0500

    v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Liam O'Neil lol
         </a>
       </header>
     </div>
